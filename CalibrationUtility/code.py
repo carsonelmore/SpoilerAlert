dcb87e37fd6d5caf8bd85aa96e7fa2f84b663802
carsonelmore <42653193+carsonelmore@users.noreply.github.com>
2022-03-29 17:55:07 -0500

    Servo implementation

--- CalibrationUtility/code.py
+++ CalibrationUtility/code.py
@@ -11,24 +11,63 @@
 
 import time
 import board
+import pwmio
+from adafruit_motor import servo
 
 # setup
 i2c = board.I2C()
 GY521 = 0x68 # I2C DEVICE ADDRESS
 
+pwm1 = pwmio.PWMOut(board.D11, duty_cycle = 2**15, frequency = 50)
+pwm2 = pwmio.PWMOut(board.D10, duty_cycle = 2**15, frequency = 50)
+pwm3 = pwmio.PWMOut(board.A1, duty_cycle = 2**15, frequency = 50)
+pwm4 = pwmio.PWMOut(board.A2, duty_cycle = 2**15, frequency = 50)
+pwm5 = pwmio.PWMOut(board.D9, duty_cycle = 2**15, frequency = 50)
+
+servo1 = servo.Servo(pwm1)
+servo2 = servo.Servo(pwm2)
+servo3 = servo.Servo(pwm3)
+servo4 = servo.Servo(pwm4)
+servo5 = servo.Servo(pwm5)
+
 
 # main loop
+ready = True;
+while True:
+    if ready:
+        print("Enter servo number:desired angle, eg 2:045")
+    command = read(5)
+    if command is None:
+        continue
+    if(command = "STOP":
+        break
+    servo = int(command[0:1])
+    match servo:
+        case 1: 
+            servo1.angle = int(command[2:5])
+        case 2: 
+            servo2.angle = int(command[2:5])
+        case 3: 
+            servo3.angle = int(command[2:5])
+        case 4: 
+            servo4.angle = int(command[2:5])
+        case 5: 
+            servo5.angle = int(command[2:5])
+        case _:
+            print("Command not parseable. To stop, enter STOP.")
+    
+    
 
-while not i2c.try_lock():
-    pass
+# while not i2c.try_lock():
+    # pass
 
-try:
-    while True:
-        print(
-            "I2C addresses found:",
-            [hex(device_address) for device_address in i2c.scan()],
-        )
-        time.sleep(2)
+# try:
+    # while True:
+        # print(
+            # "I2C addresses found:",
+            # [hex(device_address) for device_address in i2c.scan()],
+        # )
+        # sleep(2)
 
-finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
-    i2c.unlock()
+# finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
+    # i2c.unlock()
