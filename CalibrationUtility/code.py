019f3daa4f5cf275e8f5a2881e05e54ab301d7e1
carsonelmore <42653193+carsonelmore@users.noreply.github.com>
2022-03-29 22:56:22 -0500

    PWMtest running
    Changes for CircuitPython 7 (Python 3.4)

--- CalibrationUtility/code.py
+++ CalibrationUtility/code.py
@@ -13,22 +13,11 @@ import time
 import board
 import pwmio
 from adafruit_motor import servo
+import usb_cdc
 
 # setup
 i2c = board.I2C()
 GY521 = 0x68 # I2C DEVICE ADDRESS
-
-pwm1 = pwmio.PWMOut(board.D11, duty_cycle = 2**15, frequency = 50)
-pwm2 = pwmio.PWMOut(board.D10, duty_cycle = 2**15, frequency = 50)
-pwm3 = pwmio.PWMOut(board.A1, duty_cycle = 2**15, frequency = 50)
-pwm4 = pwmio.PWMOut(board.A2, duty_cycle = 2**15, frequency = 50)
-pwm5 = pwmio.PWMOut(board.D9, duty_cycle = 2**15, frequency = 50)
-
-servo1 = servo.Servo(pwm1)
-servo2 = servo.Servo(pwm2)
-servo3 = servo.Servo(pwm3)
-servo4 = servo.Servo(pwm4)
-servo5 = servo.Servo(pwm5)
 
 
 # main loop
@@ -36,25 +25,44 @@ ready = True;
 while True:
     if ready:
         print("Enter servo number:desired angle, eg 2:045")
-    command = read(5)
+    command = usb_cdc.console.read(5)
     if command is None:
         continue
-    if(command = "STOP":
+    if command is "STOP":
         break
-    servo = int(command[0:1])
-    match servo:
-        case 1: 
+    whichServo = int(command[0:1])
+    print("Parse: servo ", whichServo, "\n       Angle ", int(command[2:5]))
+    try:
+        if whichServo is 1:
+            pwm1 = pwmio.PWMOut(board.D11, duty_cycle = 2**15, frequency = 50)
+            servo1 = servo.Servo(pwm1)
             servo1.angle = int(command[2:5])
-        case 2: 
+            pwm1.deinit()
+        elif whichServo is 2:
+            pwm2 = pwmio.PWMOut(board.D10, duty_cycle = 2**15, frequency = 50)
+            servo2 = servo.Servo(pwm2)
             servo2.angle = int(command[2:5])
-        case 3: 
+            pwm2.deinit()
+        elif whichServo is 3:
+            pwm3 = pwmio.PWMOut(board.A1, duty_cycle = 2**15, frequency = 50)
+            servo3 = servo.Servo(pwm3)
             servo3.angle = int(command[2:5])
-        case 4: 
+            pwm3.deinit()
+        elif whichServo is 4:
+            pwm4 = pwmio.PWMOut(board.A2, duty_cycle = 2**15, frequency = 50)
+            servo4 = servo.Servo(pwm4)
             servo4.angle = int(command[2:5])
-        case 5: 
+            pwm4.deinit()
+        elif whichServo is 5:
+            pwm5 = pwmio.PWMOut(board.D9, duty_cycle = 2**15, frequency = 50)
+            servo5 = servo.Servo(pwm5)
             servo5.angle = int(command[2:5])
-        case _:
+            pwm5.deinit()
+        else:
             print("Command not parseable. To stop, enter STOP.")
+    except ValueError as err:
+        print(command, " is a number out of range")
+        print(err)
     
     
 
